3de68e22cfd56a9f277235c78196fa227bcfd4e0
Caposto <christiand.apostol@outlook.com>
2023-12-15 18:02:38 -0500

    solution for day 1 part 2

--- day_1/trebuchet.js
+++ day_1/trebuchet.js
@@ -3,6 +3,23 @@ const readline = require('readline');
 
 const digits = new Set(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
 
+// Part 2: Replace all words with digits
+// The reason you don't just replace with the digits is because the order in which the digits should be replaced is never established
+// i.e the letters of "three" specifically the "t" and "e" might have been part of another number
+function replaceWithDigits(line) {
+  let newLine = line;
+  newLine = newLine.replace(/one/g, "o1e");
+  newLine = newLine.replace(/two/g, "t2o");
+  newLine = newLine.replace(/three/g, "t3e");
+  newLine = newLine.replace(/four/g, "4");
+  newLine = newLine.replace(/five/g, "5e");
+  newLine = newLine.replace(/six/g, "6");
+  newLine = newLine.replace(/seven/g, "7n");
+  newLine = newLine.replace(/eight/g, "e8");
+  newLine = newLine.replace(/nine/g, "9");
+  return newLine;
+}
+
 async function processInputByLine(filePath) {
   let sum = 0;
   const fileStream = fs.createReadStream(filePath);
@@ -13,17 +30,19 @@ async function processInputByLine(filePath) {
   });
 
   for await (const line of rl) {
+    const newLine = replaceWithDigits(line);
+    const newLineReversed = newLine.split("").reverse().join("");
     let first;
     let second;
 
-    for await (const ch of line) {
+    for await (const ch of newLine) {
       if (digits.has(ch)) {
         first = ch;
         break;
       }
     }
 
-    for await (const ch of line.split("").reverse().join("")) {
+    for await (const ch of newLineReversed) {
       if (digits.has(ch)) {
         second = ch;
         break;
@@ -37,4 +56,7 @@ async function processInputByLine(filePath) {
   console.log(sum);
 }
 
-console.log(processInputByLine("input.txt")); // Answer 55712
+console.log(processInputByLine("input.txt")); 
+
+// Part 1 : 55712
+// Part 2 : 55413 (Mine is Wrong)
