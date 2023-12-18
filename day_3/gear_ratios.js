928df9a26daff9980d17ad7fdd72a7a10f00a880
Christian Apostol <94158310+Caposto@users.noreply.github.com>
2023-12-17 22:59:24 -0500

    Day3 (#1)
    * parsed input for day3
    
    * tentative function getting wrong answer
    
    * issue with the visited set and execution of part numbers
    
    * smaller input for debugging
    
    * incorrect moving on

---
+++ day_3/gear_ratios.js
@@ -0,0 +1,85 @@
+/* Parse input into a 2D matrix
+ * Adjacency defined as any number to the left, right, top, bottom, or diagonal of the number
+ * Only 1 digit in the whole number has to be adjacent to a non-numerical symbol
+ * Number of islands BFS?
+ */
+
+const fs = require('fs');
+const readline = require('readline');
+
+async function createGraphFromInput(filePath) {
+  const fileStream = fs.createReadStream(filePath);
+
+  const lineQueue = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity, // Default 100 ms
+  });
+
+  let gearGraph = [];
+
+  for await (const line of lineQueue) {
+    const graphRow = line.split("");
+    gearGraph.push(graphRow);
+  }
+
+  return gearGraph;
+}
+
+function sumEngineParts(engineSchematic) {
+  const ROWS = engineSchematic.length;
+  const COLS = engineSchematic[0].length;
+  const visited = new Set();
+  const digits = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+
+  // [row, col] : right, left, up, down, upper/right, bottom/right, upper/left, bottom/left
+  const directions =[[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]];
+  let partSum = 0;
+
+  // Helper function that gets the full engine number given a row from the schematic and the index detected
+  function getFullEngineNumber(engineSchematicRow, c, r) {
+    visited.add(`${r},${c}`);
+    let left = c;
+    let right = c;
+
+    while (left - 1 > 0 && digits.has(engineSchematicRow[left - 1])) {
+      left--; 
+      visited.add(`${r},${left}`);
+    }
+
+    while (right + 1 < COLS && digits.has(engineSchematicRow[right + 1])) {
+      right++;
+      visited.add(`${r},${right}`);
+    }
+
+    const enginePartNumber = Number(engineSchematicRow.slice(left, right + 1).join(''));
+    console.log(enginePartNumber);
+    return enginePartNumber;
+  }
+
+  for (let r = 0; r < ROWS; r++) {
+    for (let c = 0; c < COLS; c++) {
+      // Check if the current part is a symbol
+      if (engineSchematic[r][c] != "." && !digits.has(engineSchematic[r][c])) {
+        for (let i = 0; i < directions.length; i++) {
+          let new_r = r + directions[i][0];
+          let new_c = c + directions[i][1];
+
+          if (visited.has(`${new_r},${new_c}`) || new_r < 0 || new_r >= ROWS || new_c < 0 || new_c >= COLS) {
+            continue;
+          }
+
+          if (digits.has(engineSchematic[new_r][new_c])) {
+            partSum += getFullEngineNumber(engineSchematic[new_r], new_c, new_r);
+          }
+        }
+      }
+    }
+  }
+  return partSum;
+}
+
+createGraphFromInput("smaller_input.txt")
+  .then(function(result) {
+    console.log(sumEngineParts(result));
+  }
+);
